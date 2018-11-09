02efcc03d7b4952b5273e55c7d7149c9b657b69a
maxim <maxim.ryklis@spsmail.cuny.edu>
2018-11-09 10:45:07 -0500

    update session

--- app/models.py
+++ app/models.py
@@ -1,13 +1,23 @@
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,3 +31,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
