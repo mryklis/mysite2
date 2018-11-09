cf290ff28656125eb590ea0ecfa9507af17f4e62
maxim <maxim.ryklis@spsmail.cuny.edu>
2018-11-09 14:07:52 -0500

    update css

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 from datetime import datetime
 from flask_login import UserMixin
 from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -34,9 +34,6 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        # if not next_page or url_parse(next_page).netloc != '':
-        #     next_page = url_for('index')
-        #     return redirect(next_page)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
